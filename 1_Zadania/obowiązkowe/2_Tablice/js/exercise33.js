c57128272d179df105cf95a2517f44dcd6129dd6
Besaraba Maciej <maciej.besaraba@microsoft.wsei.edu.pl>
2019-10-06 12:40:05 +0200

    exercise33

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/exercise33.js
@@ -0,0 +1,17 @@
+//zadanie 2
+var favouritFruits[0]);
+console.log(favouriteFruits[0]);
+console.log(favouritFruits[favouritFruits.length - 1]);
+
+for (var i = 0; i < favouritFruits.length; i++) {
+    console.log(favouritFruits[i]);
+}
+
+//zadanie 3
+vat tab = [1,2,3,4]
+function printTable(array){
+    for(var i=0; i<array.length; i++){
+        console.log(array[i]);
+    }
+}
+printTable(tab)
