6dafc2117ec9adf3a097627c3a232291f4946817
Besaraba Maciej <maciej.besaraba@microsoft.wsei.edu.pl>
2019-12-08 12:31:28 +0100

    add ex0-3

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -12,4 +12,27 @@ function getDataAnimation(element) {
 
 console.log(getDataAnimation(titleE1));
 
+const home = document.querySelector("#home");
+const home2 = document.queryElementbyId("home2");
+const lielement = document.querySelector("li[data-direction]");
+const element = document.querySelector(".block");
+
+const list = document.querySelectorAll("nav li");
+const paragraphs = document.querySelectorAll("div p");
+const divs = document.querySelectorAll("article div");
+
+console.table(list);
+console.table(paragraphs);
+console.table(divs);
+
+const article = document.querySelector("article.first");
+let result = article.getElementsByTagName("h1");
+console.log(result.length);
+
 });
+
+
+
+
+
+
