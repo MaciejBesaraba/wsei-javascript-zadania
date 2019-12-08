5cda5c9edc2d52f8a79c76077406db946b516465
Besaraba Maciej <maciej.besaraba@microsoft.wsei.edu.pl>
2019-12-08 12:02:36 +0100

    add ex0

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+
+const titleE1 = document.querySelector("div .title");
+
+function getDataAnimation(element) {
+    return element.getAttribute("data-animation");
+}
+
+console.log(getDataAnimation(titleE1));
+
 });
