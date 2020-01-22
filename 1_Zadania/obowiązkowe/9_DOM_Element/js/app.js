534dbb8fedf496037dd276f4d610d44e6cc70e99
MaciejBesaraba <maciej.besaraba@microsoft.wsei.edu.pl>
2020-01-22 15:34:02 +0100

    add subject 9 tasks

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,77 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    // Task 0
+    function getDatasInfo(elements) {
+        return Array.from(elements).map(el => el.dataset);
+    }
+
+    getDatasInfo(links);
+
+    // Task 1
+    logHtmlElement(homeElement);
+    logHtmlCollection(childElements);
+    logHtmlElement(banner);
+    logHtmlCollection(blocks);
+    logHtmlCollection(links);
+
+    function logHtmlElement(htmlElement, prefix='\t') {
+        console.log(htmlElement);
+        console.log(`${prefix}tag=${htmlElement.tagName}`);
+        console.log(`${prefix}class=${htmlElement.className}`);
+    }
+
+    function logHtmlCollection(htmlCollection) {
+        console.log(htmlCollection);
+        Array.from(htmlCollection).forEach(element => {
+            logHtmlElement(element, '\t\t');
+        });
+    }
+
+    // Task 2
+    blocks.forEach(block => {
+        console.log(`innerHtml=${block.innerHTML}`);
+        console.log(`outerHtml=${block.outerHTML}`);
+        block.innerHTML = `Nowa wartość diva o klasie blocks`;
+    });
+
+    // InnerHtml returns content of html element as string
+    // OuterHtml on top of InnerHtml also includes html element's own tag with attributes
+
+    // Task 3
+    const mainFooter = document.getElementById('mainFooter');
+
+    function getId(element) {
+        return element.id;
+    }
+
+    getId(mainFooter);
+
+    // Task 4
+    function getTags(elements) {
+        return Array.from(elements).map(el => el.tagName);
+    }
+
+    getTags(childElements);
+
+    // Task 5
+    function getClassInfo(element) {
+        // TODO not a return expected
+        return element.classList;
+    }
+
+    getClassInfo(banner);
+
+    // Task 6
+    const liElements = document.querySelectorAll('nav li');
+
+    function setDataDirection(elements) {
+        Array.from(elements).forEach(el => {
+            if (!el.getAttribute('data-direction')) {
+                el.setAttribute('data-direction', 'top');
+            }
+        });
+    }
+
+    setDataDirection(liElements);
 });
