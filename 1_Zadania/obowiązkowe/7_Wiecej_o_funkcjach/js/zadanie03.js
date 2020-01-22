dddad5d13447a91ca82469eafffacbc290d24502
MaciejBesaraba <maciej.besaraba@microsoft.wsei.edu.pl>
2020-01-22 20:35:30 +0100

    add task 3

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
@@ -1,11 +1,30 @@
-a ()
 
-function a(){
-    console.log("Cześć");
+function hello() {
+    console.log('hello1, deklaracja przed wywołaniem');
 }
-a()
-b()
-var b = function (){
-    console.log("Witaj");
+
+hello();
+hello2();
+
+function hello2() {
+    console.log('hello2, deklaracja po wywołaniu');
 }
-b()
+
+//obie funkcje są wywoływane niezależnie od kolejności wykonania w pliku
+
+
+if (helloAsVariable !== undefined) {
+    helloAsVariable();
+} else {
+    console.log('could not call function as variable before its created');
+}
+
+
+var helloAsVariable = function() {
+    console.log('hello, as a function stored in variable');
+}
+
+helloAsVariable();
+
+//zmienna tworząca funkcje nie jest jeszcze stworzona, więc nie ma
+//odniesienia do funkcji, która mogłaby być wywołana
